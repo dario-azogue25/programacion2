57d8aa5f253993c8b0051f2c9bbd0299555ca179
dario-azogue25 <darioazoguezardan@gmail.com>
2026-03-13 08:46:42 -0400

    Add files via upload

---
+++ calc/src/Calculador.java
@@ -0,0 +1,18 @@
+public class Calculador {
+    int a = 5;
+    int b = 6;
+
+    public static int suma(int a, int b, int c) {
+        return a+b+c;
+    }
+    public static int resta(int a, int b, int c) {
+        return a-b-c;
+    }
+    public static int multiplicacion(int a, int b, int c) {
+        return a*b*c;
+    }
+    public static int division(int a, int b, int c) {
+        return a/b/c;
+    }
+
+}
---
+++ calc/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("La suma es: " + Calculador.suma(3,2,7));
+        System.out.println("La resta es: " + Calculador.resta(8,6,4));
+        System.out.println("La multiplicacion es: " + Calculador.multiplicacion(3,4,9));
+        System.out.println("La division es: " + Calculador.division(12,4,3));
+    }
+}
